688d73b28ecddbe821a7a693003a42c47ffa5057
Akhila-tech22 <akhilahima22@gmail.com>
2025-10-31 20:17:12 +0530

    Create search.js

---
+++ BST/search.js
@@ -0,0 +1,73 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    
+    insert(data) {
+        let newNode = new Node(data);
+        if(!this.root) {
+            this.root = newNode;
+            return;
+        }
+        let current = this.root;
+        
+        while(true) {
+            if(data === current.data) return;
+            if(data < current.data) {
+                if(!current.left) {
+                    current.left = newNode;
+                    return;
+                }
+                current = current.left;
+            } else {
+                if(!current.right) {
+                    current.right = newNode;
+                    return;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    inOrder() {
+        let res = [];
+        function call(node) {
+            if(!node) return;
+            call(node.left);
+            res.push(node.data);
+            call(node.right);
+        }
+        call(this.root);
+        return res;
+    }
+
+   search(data) {
+       let current = this.root;
+       while(current) {
+           if(data === current.data) return true;
+           if(data < current.data) {
+               current = current.left;
+           } else {
+               current = current.right;
+           }
+       }
+       return false;
+   }
+}
+
+const tree = new BST();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(12);
+
+console.log(tree.search(5));     
+
