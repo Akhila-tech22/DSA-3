7b0b0cc193912ef61842d90f9c8b8b39b8984046
Akhila-tech22 <akhilahima22@gmail.com>
2025-10-31 20:55:48 +0530

    Create count one childNode.js

---
+++ BST/count one childNode.js
@@ -0,0 +1,68 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    
+    insert(data) {
+        let newNode = new Node(data);
+        if(!this.root) {
+            this.root = newNode;
+            return;
+        }
+        let current = this.root;
+        
+        while(true) {
+            if(data === current.data) return;
+            if(data < current.data) {
+                if(!current.left) {
+                    current.left = newNode;
+                    return;
+                }
+                current = current.left;
+            } else {
+                if(!current.right) {
+                    current.right = newNode;
+                    return;
+                }
+                current = current.right;
+            }
+        }
+    }
+
+    inOrder() {
+        let res = [];
+        function call(node) {
+            if(!node) return;
+            call(node.left);
+            res.push(node.data);
+            call(node.right);
+        }
+        call(this.root);
+        return res;
+    }
+    count(node = this.root) {
+        if(!node) return 0;
+        let count = 0;
+        if((node.left && !node.right) || (!node.left && node.right)) {
+            count++;
+        }
+        return count + this.count(node.left) + this.count(node.right)
+    }
+}
+
+const tree = new BST();
+tree.insert(10);
+tree.insert(5);
+tree.insert(15);
+tree.insert(12);
+
+console.log(tree.count())
+
