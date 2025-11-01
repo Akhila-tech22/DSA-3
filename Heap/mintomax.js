40bd73e0af25182c5b96e2e0d0f00d5df589df4b
Akhila-tech22 <akhilahima22@gmail.com>
2025-11-01 08:56:25 +0530

    Create mintomax.js

---
+++ Heap/mintomax.js
@@ -0,0 +1,30 @@
+function convertMinToMax(heap) {
+     let n = heap.length;
+
+     for (let i = Math.floor(n / 2) - 1; i >= 0; i--) {
+          maxHeapify(heap, n, i)
+     }
+}
+
+function maxHeapify(heap, n, i) {
+     let left = (2 * i) + 1;
+     let right = (2 * i) + 2;
+     let largest = i;
+
+     if (left < n && heap[left] > heap[largest]) {
+          largest = left;
+     }
+     if (right < n && heap[right] > heap[largest]) {
+          largest = right;
+     }
+     if (largest !== i) {
+          [heap[i], heap[largest]] = [heap[largest], heap[i]]
+          maxHeapify(heap, n, largest)
+     }
+}
+
+let minHeapArr = [10, 12, 14, 25]  // [ 25, 12, 14, 10 ]
+convertMinToMax(minHeapArr);
+console.log("Back to Max Heap:", minHeapArr);
+
+
